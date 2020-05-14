8ad5f7752628ab4021f62ebe2308ecd5aa07dcc8
viktor kjeldal <vikt5485@stud.kea.dk>
2020-05-14 15:07:59 +0200

    taps virker med data

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -117,79 +117,110 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
-var bundleURL = null;
+})({"main.js":[function(require,module,exports) {
+"use strict";
 
-function getBundleURLCached() {
-  if (!bundleURL) {
-    bundleURL = getBundleURL();
-  }
+document.addEventListener("DOMContentLoaded", start);
+var endPoint = "https://examfoobar.herokuapp.com/";
+var HTML = {};
 
-  return bundleURL;
+function start() {
+  console.log("START");
+  HTML.tap0 = document.querySelector("#tapcontainer article:nth-child(1)");
+  HTML.tap1 = document.querySelector("#tapcontainer article:nth-child(2)");
+  HTML.tap2 = document.querySelector("#tapcontainer article:nth-child(3)");
+  HTML.tap3 = document.querySelector("#tapcontainer article:nth-child(4)");
+  HTML.tap4 = document.querySelector("#tapcontainer article:nth-child(5)");
+  HTML.tap5 = document.querySelector("#tapcontainer article:nth-child(6)");
+  HTML.tap6 = document.querySelector("#tapcontainer article:nth-child(7)");
+  fetchData();
 }
 
-function getBundleURL() {
-  // Attempt to find the URL of the current script and use that as the base URL
-  try {
-    throw new Error();
-  } catch (err) {
-    var matches = ('' + err.stack).match(/(https?|file|ftp|chrome-extension|moz-extension):\/\/[^)\n]+/g);
+function fetchData() {
+  fetch(endPoint, {
+    method: "get"
+  }).then(function (data) {
+    return data.json();
+  }).then(function (data) {
+    showData(data);
+    setTimeout(fetchData, 500);
+  });
+}
 
-    if (matches) {
-      return getBaseURL(matches[0]);
+function showData(data) {
+  console.log(data); //TAP 1
+
+  HTML.tap0.querySelector("h3").textContent = data.taps[0].beer;
+  HTML.tap0.setAttribute("data-beertype", data.taps[0].beer);
+  HTML.tap0.querySelector(".overlay").style.height = data.taps[0].level / data.taps[0].capacity * 100 + "%";
+  HTML.tap0.querySelector(".level").textContent = data.taps[0].level / 100 + "L";
+  data.storage.forEach(function (beer) {
+    if (beer.name === data.taps[0].beer) {
+      HTML.tap0.querySelector(".storage").textContent = beer.amount + " x";
     }
-  }
+  }); //TAP 2
 
-  return '/';
-}
-
-function getBaseURL(url) {
-  return ('' + url).replace(/^((?:https?|file|ftp|chrome-extension|moz-extension):\/\/.+)\/[^/]+$/, '$1') + '/';
-}
-
-exports.getBundleURL = getBundleURLCached;
-exports.getBaseURL = getBaseURL;
-},{}],"node_modules/parcel-bundler/src/builtins/css-loader.js":[function(require,module,exports) {
-var bundle = require('./bundle-url');
-
-function updateLink(link) {
-  var newLink = link.cloneNode();
-
-  newLink.onload = function () {
-    link.remove();
-  };
-
-  newLink.href = link.href.split('?')[0] + '?' + Date.now();
-  link.parentNode.insertBefore(newLink, link.nextSibling);
-}
-
-var cssTimeout = null;
-
-function reloadCSS() {
-  if (cssTimeout) {
-    return;
-  }
-
-  cssTimeout = setTimeout(function () {
-    var links = document.querySelectorAll('link[rel="stylesheet"]');
-
-    for (var i = 0; i < links.length; i++) {
-      if (bundle.getBaseURL(links[i].href) === bundle.getBundleURL()) {
-        updateLink(links[i]);
-      }
+  HTML.tap1.querySelector("h3").textContent = data.taps[1].beer;
+  HTML.tap1.setAttribute("data-beertype", data.taps[1].beer);
+  HTML.tap1.querySelector(".overlay").style.height = data.taps[1].level / data.taps[1].capacity * 100 + "%";
+  HTML.tap1.querySelector(".level").textContent = data.taps[1].level / 100 + "L";
+  data.storage.forEach(function (beer) {
+    if (beer.name === data.taps[1].beer) {
+      HTML.tap1.querySelector(".storage").textContent = beer.amount + " x";
     }
+  }); //TAP 3
 
-    cssTimeout = null;
-  }, 50);
+  HTML.tap2.querySelector("h3").textContent = data.taps[2].beer;
+  HTML.tap2.setAttribute("data-beertype", data.taps[2].beer);
+  HTML.tap2.querySelector(".overlay").style.height = data.taps[2].level / data.taps[2].capacity * 100 + "%";
+  HTML.tap2.querySelector(".level").textContent = data.taps[2].level / 100 + "L";
+  data.storage.forEach(function (beer) {
+    if (beer.name === data.taps[2].beer) {
+      HTML.tap2.querySelector(".storage").textContent = beer.amount + " x";
+    }
+  }); //TAP 4
+
+  HTML.tap3.querySelector("h3").textContent = data.taps[3].beer;
+  HTML.tap3.setAttribute("data-beertype", data.taps[3].beer);
+  HTML.tap3.querySelector(".overlay").style.height = data.taps[3].level / data.taps[3].capacity * 100 + "%";
+  HTML.tap3.querySelector(".level").textContent = data.taps[3].level / 100 + "L";
+  data.storage.forEach(function (beer) {
+    if (beer.name === data.taps[3].beer) {
+      HTML.tap3.querySelector(".storage").textContent = beer.amount + " x";
+    }
+  }); //TAP 5
+
+  HTML.tap4.querySelector("h3").textContent = data.taps[4].beer;
+  HTML.tap4.setAttribute("data-beertype", data.taps[4].beer);
+  HTML.tap4.querySelector(".overlay").style.height = data.taps[4].level / data.taps[4].capacity * 100 + "%";
+  HTML.tap4.querySelector(".level").textContent = data.taps[4].level / 100 + "L";
+  data.storage.forEach(function (beer) {
+    if (beer.name === data.taps[4].beer) {
+      HTML.tap4.querySelector(".storage").textContent = beer.amount + " x";
+    }
+  }); //TAP 6
+
+  HTML.tap5.querySelector("h3").textContent = data.taps[5].beer;
+  HTML.tap5.setAttribute("data-beertype", data.taps[5].beer);
+  HTML.tap5.querySelector(".overlay").style.height = data.taps[5].level / data.taps[5].capacity * 100 + "%";
+  HTML.tap5.querySelector(".level").textContent = data.taps[5].level / 100 + "L";
+  data.storage.forEach(function (beer) {
+    if (beer.name === data.taps[5].beer) {
+      HTML.tap5.querySelector(".storage").textContent = beer.amount + " x";
+    }
+  }); //TAP 7
+
+  HTML.tap6.querySelector("h3").textContent = data.taps[6].beer;
+  HTML.tap6.setAttribute("data-beertype", data.taps[6].beer);
+  HTML.tap6.querySelector(".overlay").style.height = data.taps[6].level / data.taps[6].capacity * 100 + "%";
+  HTML.tap6.querySelector(".level").textContent = data.taps[6].level / 100 + "L";
+  data.storage.forEach(function (beer) {
+    if (beer.name === data.taps[6].beer) {
+      HTML.tap6.querySelector(".storage").textContent = beer.amount + " x";
+    }
+  });
 }
-
-module.exports = reloadCSS;
-},{"./bundle-url":"node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"styles.scss":[function(require,module,exports) {
-var reloadCSS = require('_css_loader');
-
-module.hot.dispose(reloadCSS);
-module.hot.accept(reloadCSS);
-},{"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -393,5 +424,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
-//# sourceMappingURL=/styles.164d45a1.js.map
+},{}]},{},["node_modules/parcel-bundler/src/builtins/hmr-runtime.js","main.js"], null)
+//# sourceMappingURL=/main.1f19ae8e.js.map
---
+++ main.js
@@ -0,0 +1,131 @@
+"use strict";
+document.addEventListener("DOMContentLoaded", start);
+
+const endPoint = "https://examfoobar.herokuapp.com/";
+const HTML = {};
+
+function start() {
+  console.log("START");
+  HTML.tap0 = document.querySelector("#tapcontainer article:nth-child(1)");
+  HTML.tap1 = document.querySelector("#tapcontainer article:nth-child(2)");
+  HTML.tap2 = document.querySelector("#tapcontainer article:nth-child(3)");
+  HTML.tap3 = document.querySelector("#tapcontainer article:nth-child(4)");
+  HTML.tap4 = document.querySelector("#tapcontainer article:nth-child(5)");
+  HTML.tap5 = document.querySelector("#tapcontainer article:nth-child(6)");
+  HTML.tap6 = document.querySelector("#tapcontainer article:nth-child(7)");
+
+  fetchData();
+}
+
+function fetchData() {
+  fetch(endPoint, {
+    method: "get",
+  })
+    .then((data) => data.json())
+    .then((data) => {
+      showData(data);
+      setTimeout(fetchData, 500);
+    });
+}
+
+function showData(data) {
+  console.log(data);
+
+  //TAP 1
+  HTML.tap0.querySelector("h3").textContent = data.taps[0].beer;
+  HTML.tap0.setAttribute("data-beertype", data.taps[0].beer);
+  HTML.tap0.querySelector(".overlay").style.height =
+    (data.taps[0].level / data.taps[0].capacity) * 100 + "%";
+  HTML.tap0.querySelector(".level").textContent =
+    data.taps[0].level / 100 + "L";
+
+  data.storage.forEach((beer) => {
+    if (beer.name === data.taps[0].beer) {
+      HTML.tap0.querySelector(".storage").textContent = beer.amount + " x";
+    }
+  });
+
+  //TAP 2
+  HTML.tap1.querySelector("h3").textContent = data.taps[1].beer;
+  HTML.tap1.setAttribute("data-beertype", data.taps[1].beer);
+  HTML.tap1.querySelector(".overlay").style.height =
+    (data.taps[1].level / data.taps[1].capacity) * 100 + "%";
+  HTML.tap1.querySelector(".level").textContent =
+    data.taps[1].level / 100 + "L";
+
+  data.storage.forEach((beer) => {
+    if (beer.name === data.taps[1].beer) {
+      HTML.tap1.querySelector(".storage").textContent = beer.amount + " x";
+    }
+  });
+
+  //TAP 3
+  HTML.tap2.querySelector("h3").textContent = data.taps[2].beer;
+  HTML.tap2.setAttribute("data-beertype", data.taps[2].beer);
+  HTML.tap2.querySelector(".overlay").style.height =
+    (data.taps[2].level / data.taps[2].capacity) * 100 + "%";
+  HTML.tap2.querySelector(".level").textContent =
+    data.taps[2].level / 100 + "L";
+
+  data.storage.forEach((beer) => {
+    if (beer.name === data.taps[2].beer) {
+      HTML.tap2.querySelector(".storage").textContent = beer.amount + " x";
+    }
+  });
+
+  //TAP 4
+  HTML.tap3.querySelector("h3").textContent = data.taps[3].beer;
+  HTML.tap3.setAttribute("data-beertype", data.taps[3].beer);
+  HTML.tap3.querySelector(".overlay").style.height =
+    (data.taps[3].level / data.taps[3].capacity) * 100 + "%";
+  HTML.tap3.querySelector(".level").textContent =
+    data.taps[3].level / 100 + "L";
+
+  data.storage.forEach((beer) => {
+    if (beer.name === data.taps[3].beer) {
+      HTML.tap3.querySelector(".storage").textContent = beer.amount + " x";
+    }
+  });
+
+  //TAP 5
+  HTML.tap4.querySelector("h3").textContent = data.taps[4].beer;
+  HTML.tap4.setAttribute("data-beertype", data.taps[4].beer);
+  HTML.tap4.querySelector(".overlay").style.height =
+    (data.taps[4].level / data.taps[4].capacity) * 100 + "%";
+  HTML.tap4.querySelector(".level").textContent =
+    data.taps[4].level / 100 + "L";
+
+  data.storage.forEach((beer) => {
+    if (beer.name === data.taps[4].beer) {
+      HTML.tap4.querySelector(".storage").textContent = beer.amount + " x";
+    }
+  });
+
+  //TAP 6
+  HTML.tap5.querySelector("h3").textContent = data.taps[5].beer;
+  HTML.tap5.setAttribute("data-beertype", data.taps[5].beer);
+  HTML.tap5.querySelector(".overlay").style.height =
+    (data.taps[5].level / data.taps[5].capacity) * 100 + "%";
+  HTML.tap5.querySelector(".level").textContent =
+    data.taps[5].level / 100 + "L";
+
+  data.storage.forEach((beer) => {
+    if (beer.name === data.taps[5].beer) {
+      HTML.tap5.querySelector(".storage").textContent = beer.amount + " x";
+    }
+  });
+
+  //TAP 7
+  HTML.tap6.querySelector("h3").textContent = data.taps[6].beer;
+  HTML.tap6.setAttribute("data-beertype", data.taps[6].beer);
+  HTML.tap6.querySelector(".overlay").style.height =
+    (data.taps[6].level / data.taps[6].capacity) * 100 + "%";
+  HTML.tap6.querySelector(".level").textContent =
+    data.taps[6].level / 100 + "L";
+
+  data.storage.forEach((beer) => {
+    if (beer.name === data.taps[6].beer) {
+      HTML.tap6.querySelector(".storage").textContent = beer.amount + " x";
+    }
+  });
+}
